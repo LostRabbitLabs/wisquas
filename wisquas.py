3e6266e869b7acabfe3b0c509e70f1b09d642691
the Lost Rabbit <root@414.org>
2019-12-27 17:16:09 -0700

    Update wisquas.py

--- wisquas.py
+++ wisquas.py
@@ -338,7 +338,7 @@ print "\n\n"
 print("Payload:   webcode / length / cookies / headers / server ")
 print("==============================================================")
 
-payloads = ['web.config','trace.axd','admin/','temp/','tmp/','bin/','?id=0','api/','%','%%','&','<script>alert(1)</script>','%00','index.html','index.htm','index.php','index.jsp','index.asp','index.aspx','default.asp','default.aspx','.git','.htaccess','.htpasswd','server-status','xmlrpc.php','sitemap.xml','login.php','%'*10050]
+payloads = ['web.config','trace.axd','admin/','temp/','tmp/','uploads/','bin/','?id=0','api/','%','%%','&','<script>alert(1)</script>','%00','index.html','index.htm','index.php','index.jsp','index.asp','index.aspx','default.asp','default.aspx','home.asp','home.aspx','.git','.htaccess','.htpasswd','server-status','xmlrpc.php','sitemap.xml','login.php','%'*10050]
 
 
 for pl in payloads:
