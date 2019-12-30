be91c118bfe9549dd4ee45bbaca0ab8e811dd9b9
the Lost Rabbit <root@414.org>
2019-12-29 19:30:31 -0700

    wisquas v0.8
    wisquas v0.8

--- wisquas.py
+++ wisquas.py
@@ -338,7 +338,7 @@ print "\n\n"
 print("Payload:   webcode / length / cookies / headers / server ")
 print("==============================================================")
 
-payloads = ['index.html','index.htm','index.shtml','index.php','index.jsp','index.asp','index.aspx','default.asp','default.aspx','home.asp','home.aspx','aspnet_files/','aspnet_client/','web.config','trace.axd','admin/','temp/','tmp/','uploads/','bin/','files/','webdav/','manager/','logs/','jmx-console','INSTALL.mysql.txt','INSTALL.txt','UPGRADE.txt','wp-login.php','README','WEB-INF/','server-status','server-info','config.php','xmlrpc.php','sitemap.xml','login.php','console','status','error','phpinfo.php','access_log','php.ini','.git','.htaccess','.htpasswd','.mysql_history','.bashrc','.ssh','.history','.passwd','passwd','.hta','?id=0','api/','%','%%','&','<script>alert(1)</script>','%00','%'*10050]
+payloads = ['index.html','index.htm','index.shtml','index.php','index.jsp','index.asp','index.aspx','default.asp','default.aspx','home.asp','home.aspx','aspnet_files/','aspnet_client/','web.config','trace.axd','admin/','temp/','tmp/','uploads/','bin/','files/','webdav/','manager/','logs/','jmx-console','phpMyAdmin/','INSTALL.mysql.txt','INSTALL.txt','UPGRADE.txt','wp-login.php','README','WEB-INF/','server-status','server-info','config.php','xmlrpc.php','sitemap.xml','login.php','console','status','error','phpinfo.php','access_log','php.ini','.git','.htaccess','.htpasswd','.mysql_history','.bashrc','.ssh','.history','.passwd','passwd','.hta','?id=0','api/','%','%%','&','<script>alert(1)</script>','%00','%'*10050]
 
 
 for pl in payloads:
